214a76ea387d00c3ad62d482d82f73214552436b
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-11 13:47:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
-int main(){
-  return 0;
+#include "lib.h"
+
+using namespace std;
+
+int main()
+{
+   char a[10][20], b[1][20];
+   int i,posizione;
+   for(i=0;i<10;i++)
+   {
+       cin>>a[i];
+   }
+   cin>>b[0];
+   if(ricerca(a,b,posizione))
+   {
+       cout<<posizione<<endl;
+   }
+   else
+   {
+       cout<<"non presente"<<endl;
+   }
+   return 0;
 }
