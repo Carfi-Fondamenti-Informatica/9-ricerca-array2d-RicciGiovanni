04d1274ea62c3108203f42fc8312dcd4e583c9dc
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-11 13:46:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
-bool ricerca(char a[10][20], char b[1][20], int& posizione);
+#include<cstring>
+#include "lib.h"
+
+bool ricerca(char a[10][20], char b[1][20], int& posizione)
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        if(strcmp(a[i], (b[0])) == 0)
+        {
+            posizione=i;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        return 0;
+    }
+
+}
