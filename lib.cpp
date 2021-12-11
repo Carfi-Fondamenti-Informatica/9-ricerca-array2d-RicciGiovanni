af2a811931a05218934fb0217a0ca456aa7414b0
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-11 13:53:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,17 @@
 #include<cstring>
 #include "lib.h"
 
-bool ricerca(char a[10][20], char b[1][20], int& posizione)
+bool ricerca(char alfabeto[10][20], char parola[1][20], int& posizione)
 {
     int i;
     for(i=0;i<10;i++)
     {
-        if(strcmp(a[i], (b[0])) == 0)
+        if(strcmp(alfabeto[i], parola[0]) == 0)
         {
             posizione=i;
             return true;
         }
-        else
-        {
-            return false;
         }
-        return 0;
-    }
+    return false;
 
 }
